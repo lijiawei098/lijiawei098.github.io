cacddb3cf29b3c39ad7fe28aaac0ddad4edaab5e
LijwSeis <34309819+lijiawei098@users.noreply.github.com>
2025-05-21 22:04:34 +0800

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'news', 'CV']
+const section_names = ['home', 'publications', 'news', 'CV', 'AoyuX']
 
 
 window.addEventListener('DOMContentLoaded', event => {
